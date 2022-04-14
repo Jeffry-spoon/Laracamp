31014fdfb6d839b22f38fdb646d350e9854ad542
Jeffry <Jeffryacai@gmail.com>
2022-04-14 12:15:52 +0700

    Payment gateway test 1

--- Source Code/resources/views/emails/checkout/afterCheckout.blade.php
+++ Source Code/resources/views/emails/checkout/afterCheckout.blade.php
@@ -6,7 +6,7 @@
 
 Thank you for register on <b>{{$checkout->Camp->title}}</b>, please see payment instruction by click the button below.
 
-@component('mail::button', ['url' => route('dashboard')
+@component('mail::button', ['url' => route('dashboard')])
 My Dashboard
 @endcomponent
 
